29d4ec3136017b01798963c3ceb4f143fa7c111a
Tatyana <quininthenorth@gmail.com>
2023-03-20 21:16:54 +0400

    SqArea

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -7,10 +7,10 @@ public class SqArea {
         double l = h * k;
         double rsl = l * h;
         return rsl;
-        }
+    }
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
         double result1 = SqArea.square(6, 2);
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
-        }
+    }
 }
