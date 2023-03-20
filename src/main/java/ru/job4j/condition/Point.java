f0551c1d3e0ded5d8db9b36f2f18e6a48ddcbbf1
Tatyana <quininthenorth@gmail.com>
2023-03-20 21:02:49 +0400

    Distance

---
+++ src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class Point {
+
+    public static double distance(int x1, int y1, int x2, int y2) {
+        double rsl = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double result = Point.distance(0, 2, 0, -2);
+        System.out.println("result (0, 0) to (2, 0) " + result);
+    }
+}
+
