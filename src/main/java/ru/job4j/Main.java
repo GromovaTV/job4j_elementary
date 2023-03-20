f0c3d06f2b534f532a23c0df7e7f819f94ee8157
Tatyana <quininthenorth@gmail.com>
2023-03-20 13:22:49 +0400

    Тип данных

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,6 +2,10 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
+
+        int age = 18;
+        float length = 78.0332F;
+        boolean baby = true;
         System.out.println("Hello world");
     }
 }
