998b4b89a3510a4a4e7ae3392ab847a8840d92b9
Tatyana <quininthenorth@gmail.com>
2023-03-25 20:13:28 +0400

    Перестановка двух элементов числового массива

---
+++ src/main/java/ru/job4j/array/SwitchArray.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class SwitchArray {
+
+    public static int[] swap(int[] array, int source, int dest) {
+        int temp = array[source];
+        array[source] = array[dest];
+        array[dest] = temp;
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {1, 2, 3, 4, 5, 6};
+        int[] rsl = swap(nums, 0, 1);
+        for (int index = 0; index < rsl.length; index++) {
+            System.out.println(rsl[index]);
+        }
+    }
+
+}
