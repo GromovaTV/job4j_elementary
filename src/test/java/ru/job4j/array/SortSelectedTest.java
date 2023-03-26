82c103d03aa09680a961fea71d155b181b03553c
Tatyana <quininthenorth@gmail.com>
2023-03-26 12:44:43 +0400

    Сортировка выборкой

---
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+import org.junit.Assert;
+import org.junit.Test;
+public class SortSelectedTest {
+    @Test
+    public void whenSortArrayOf5elements() {
+        int[] data = new int[] {3, 4, 1, 2, 5};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 4, 5};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSortArrayOf3elements() {
+        int[] data = new int[] {3, 2, 5};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {2, 3, 5};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSortArrayOf6elements() {
+        int[] data = new int[] {1, 4, 3, 2, 6, 5};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 4, 5, 6};
+        Assert.assertArrayEquals(expected, result);
+    }
+}
