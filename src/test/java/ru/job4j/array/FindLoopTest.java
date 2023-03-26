d5abc0648bf372b357121e0e3468d335be07d97c
Tatyana <quininthenorth@gmail.com>
2023-03-26 11:34:28 +0400

    Поиск индекса в диапазоне

---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] data = {5, 4, 3, 2};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHasNot10ThenMinus1() {
+/*Здесь нужно дописать тест, когда в массиве не найдено число 10
+Можно искать любое другое число, корректно изменив при этом название теста*/
+        int[] data = {5, 4, 3, 2};
+        int el = 10;
+        int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenDiapasonHas2Then3() {
+        int[] data = new int[]{5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenDiapasonHasNot8ThenMinus1() {
+/*Дописать тест, когда элемент 8 в диапазоне не найден. Можно искать любой другой элемент, корректно отражая этот
+факт в названии теста.*/
+        int[] data = new int[]{5, 2, 10, 2, 4, 8};
+        int el = 8;
+        int start = 0;
+        int finish = 3;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+
+    }
+
+}
