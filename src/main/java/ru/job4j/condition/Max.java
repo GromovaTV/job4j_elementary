336fe35d3b343d86067bce7c44cf1120f0f935e9
Tatyana <quininthenorth@gmail.com>
2023-03-29 16:16:03 +0400

    checkstyle

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,25 +2,21 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int first, int second) {
-
         int result = first >= second ? first : second;
         return result;
     }
 
     public static int max(int first, int second, int third) {
-
         int result = max(first, second) >= third ? max(first, second) : third;
         return result;
     }
 
     public static int max(int first, int second, int third, int fourth) {
-
         int result = max(first, second, third) >= fourth ? max(first, second, third) : fourth;
         return result;
     }
 
     public static void main(String[] args) {
-
         int result = Max.max(-2, 2);
         System.out.println(result);
         result = Max.max(-2, 2, 3);
